fc917805d959142e205f33132055af0b37671014
TemiCodes <86966766+TemiCodes@users.noreply.github.com>
2023-10-26 18:51:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function g1() {
+    document.getElementById("audio1").play();
+}
+function g2() {
+    document.getElementById("audio2").play();
+}
+function g3() {
+    document.getElementById("audio3").play();
+}
+function g4() {
+    document.getElementById("audio4").play();
+}
+function g5() {
+    document.getElementById("audio5").play();
+}
+function g6() {
+    document.getElementById("audio6").play();
+}
+function g7() {
+    document.getElementById("audio7").play();
+}
+function g8() {
+    document.getElementById("audio8").play();
+}
+function g9() {
+    document.getElementById("audio9").play();
+}
+function g10() {
+    document.getElementById("audio10").play();
+}
+function g11() {
+    document.getElementById("audio11").play();
+}
+function g12() {
+    document.getElementById("audio12").play();
+}
+function g13() {
+    document.getElementById("audio13").play();
+}
+function g14() {
+    document.getElementById("audio14").play();
+}
+function g15() {
+    document.getElementById("audio15").play();
+}
